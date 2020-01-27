8f8ca17031caffd23d277b40fc75ed57a787f48c
jacobcalv <jacobcalvino@outlook.com>
2020-01-26 18:28:11 -0600

    added auth-model.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  
 });
 
 router.post('/login', (req, res) => {
