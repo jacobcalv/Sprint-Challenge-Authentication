c472bbc5d48ff1bf4db614f77504aea412901663
jacobcalv <jacobcalvino@outlook.com>
2020-01-26 19:00:01 -0600

    token middleware successfully connected and operational

--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+const tokenMiddleware = require('../auth/authenticate-middleware')
+
+
+router.get('/', tokenMiddleware, (req, res) => {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
